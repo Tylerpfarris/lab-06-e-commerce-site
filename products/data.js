d226a388fff0b1350f95094a34eb004a3a3605f0
Tyler Farris <Tylerpfarris@gmail.com>
2021-01-19 14:45:25 -0800

    wire frame done, product data done

---
+++ products/data.js
@@ -0,0 +1,62 @@
+export const fruits = [
+    {
+        id: 1,
+        name: 'Kiwi',
+        image: 'assets/Kiwi-fruit.png',
+        flavor: `Kiwis are sweet, refreshing fruits with a nice tartness that complements their sweetness. The riper the fruit is, the sweeter and less tart it tends to be.`,
+        color: `Brown skin with green flesh.`,
+        price: 1.00,
+        grownIn:`China, Italy and New Zealand`,
+        
+    },
+    {
+        id: 2,
+        name: `Mango`,
+        image: `assets/Mango-fruit.png`,
+        flavor: `Besides tasting sweet, they have a range of tastes from floral to citrusy depending on the mango. Some have a tropical flower flavor and aroma while others are almost orangy and fairly tart like lemon.`,
+        color: `Green, orange and red skin- with saffron colored flesh.`,
+        price: 2.50,
+        grownIn: `India, China, Thailand`,
+
+    },
+    {
+        id: 3,
+        name: `Starfruit`,
+        image: `assets/Starfruit.png`,
+        flavor: `Star fruit tastes tart and sweet. It’s best described as a cross between a pear, apple, plum, with a hint of citrus added. Unripe star fruits are more on the sour, citrus side while very ripe ones resemble plums and pineapple.`,
+        color: `Yellow to green skin- with pale yellow flesh`,
+        price: 3.50,
+        grownIn: `Malaysia, Philippines, India`,
+   
+    },
+    {
+        id: 4,
+        name: `Passion Fruit`,
+        image: `assets/Passion-Fruit.png`,
+        flavor: `Passion fruit have a fragrantly sweet taste with a pleasantly tart tang and are very juicy. What to akin it to? I don’t know. Let’s see. Maybe kiwi. Maybe pineapple.`,
+        color: `Purple to yellow kin- with golden flesh and dark seeds.`,
+        price: 3.50,
+        grownIn: `Malaysia, United States, Kenya`,
+   
+    },
+    {
+        id: 5,
+        name: `Guava`,
+        image: `assets/Guava-fruit.png`,
+        flavor: `The general taste of guava is said to be a cross between strawberries and pears, but depending on the variety, the sweet flavor will vary between mild and strong`,
+        color: `Green, yellow, pink to red skin- with yellow, pink to red flesh`,
+        price: 4.25,
+        grownIn: `India, China, Thailand`,
+   
+    },
+    {
+        id: 6,
+        name: `Lychee`,
+        image: `assets/Lychee-fruit.png`,
+        flavor: `Lychee like a grape, but with a stronger, slightly acidic touch. Some people also swear that it tastes more like a pear or a watermelon. It's a balance of sweet and tart.`,
+        color: `The skin is generally red or pink, covered with small wrinkled protuberances, resembling the strawberry tree fruit. The pulp is white, firm and somewhat hard, carrying a seed inside.`,
+        price: 4.25,
+        grownIn: `Taiwan, China, Thailand`,
+   
+    },
+];
