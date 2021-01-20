440a50bcd80f918a1eda2ffffee8b4bfa975ebb5
Tyler Farris <Tylerpfarris@gmail.com>
2021-01-19 16:45:23 -0800

    product list is generating!

--- products/app.js
+++ products/app.js
@@ -1,5 +1,13 @@
 // import functions and grab DOM elements
-
+import { renderFruit } from '../products/render-fruits.js';
+import { fruits } from '../products/data.js';
 // initialize state
 
+const fruitList = document.getElementById('list');
 // set event listeners to update state and DOM
+
+for (const fruit of fruits) {
+    const fruitElement = renderFruit(fruit);
+
+    fruitList.append(fruitElement);
+}
--- products/data.js
+++ products/data.js
@@ -12,7 +12,7 @@ export const fruits = [
     {
         id: 2,
         name: `Mango`,
-        image: `assets/Mango-fruit.png`,
+        image: `Mango-fruit.png`,
         flavor: `Besides tasting sweet, they have a range of tastes from floral to citrusy depending on the mango. Some have a tropical flower flavor and aroma while others are almost orangy and fairly tart like lemon.`,
         color: `Green, orange and red skin- with saffron colored flesh.`,
         price: 2.50,
@@ -22,7 +22,7 @@ export const fruits = [
     {
         id: 3,
         name: `Starfruit`,
-        image: `assets/Starfruit.png`,
+        image: `Starfruit.png`,
         flavor: `Star fruit tastes tart and sweet. It’s best described as a cross between a pear, apple, plum, with a hint of citrus added. Unripe star fruits are more on the sour, citrus side while very ripe ones resemble plums and pineapple.`,
         color: `Yellow to green skin- with pale yellow flesh`,
         price: 3.50,
@@ -32,7 +32,7 @@ export const fruits = [
     {
         id: 4,
         name: `Passion Fruit`,
-        image: `assets/Passion-Fruit.png`,
+        image: `Passion-Fruit.png`,
         flavor: `Passion fruit have a fragrantly sweet taste with a pleasantly tart tang and are very juicy. What to akin it to? I don’t know. Let’s see. Maybe kiwi. Maybe pineapple.`,
         color: `Purple to yellow kin- with golden flesh and dark seeds.`,
         price: 3.50,
@@ -42,7 +42,7 @@ export const fruits = [
     {
         id: 5,
         name: `Guava`,
-        image: `assets/Guava-fruit.png`,
+        image: `Guava-fruit.png`,
         flavor: `The general taste of guava is said to be a cross between strawberries and pears, but depending on the variety, the sweet flavor will vary between mild and strong`,
         color: `Green, yellow, pink to red skin- with yellow, pink to red flesh`,
         price: 4.25,
@@ -52,7 +52,7 @@ export const fruits = [
     {
         id: 6,
         name: `Lychee`,
-        image: `assets/Lychee-fruit.png`,
+        image: `Lychee-fruit.png`,
         flavor: `Lychee like a grape, but with a stronger, slightly acidic touch. Some people also swear that it tastes more like a pear or a watermelon. It's a balance of sweet and tart.`,
         color: `The skin is generally red or pink, covered with small wrinkled protuberances, resembling the strawberry tree fruit. The pulp is white, firm and somewhat hard, carrying a seed inside.`,
         price: 4.25,
